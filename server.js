6a88929fc3a776880d63588b3ed5939f49fa0d69
RCVehicles <david2000.zakelijk@gmail.com>
2026-04-30 15:23:03 +0000

    update

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post("/bestelling", async (req, res) => {
   }
 });
  
-app.listen(process.env.PORT || 1000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server draait");
 });
